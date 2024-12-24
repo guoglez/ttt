878333874d3d91edbd53a399e901b3fefd2eb7b4
Guofeng Chen <guofengchen1234@gmail.com>
2024-12-24 14:39:23 -0600

    backend checkpoint

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 var path = require('path');
+var cookieParser = require('cookie-parser');
 
 const app = express();
+app.use(cookieParser());
+
 let phase = 'PlayerInput';
 let tokenTurn = 'o';
 let board = [
@@ -11,8 +14,18 @@ let board = [
 ]
 let tilesfilled = 0;
 let winner = '';
+let tokeno = 'available'
+let tokenx = 'avaliable'
 
 app.use(express.json());
+
+let treasureChest = {
+    players: [],
+    phase: phase,
+    tokenTurn: tokenTurn,
+    board: board,
+    winner: winner,
+}
 
 app.use((err, req, res, next) => {
     if(err){
@@ -23,7 +36,11 @@ app.use((err, req, res, next) => {
 })
 
 app.get('/game-state', function (req, res) {
-    // res.sendFile(path.resolve('../frontend/index.html'));
+    // var hesCooking = req.cookies.letHimCook;
+    // if(!hesCooking) {
+    //     res.cookie('letHimCook', 'deznuts', { maxAge: 600000, httpOnly: true });
+    // }
+
     let gameState = {
         phase: phase,
         tokenTurn: tokenTurn,
@@ -34,8 +51,37 @@ app.get('/game-state', function (req, res) {
 })
 app.use(express.static(path.resolve('../frontend')));
 
+app.post('/enter-game', function (req, res) {
+    var username = req.cookies.username;
+    res.cookie('username', req.body.username, { maxAge: 600000, httpOnly: true });
+    treasureChest.players.push({name: username, type: null}); 
+    res.end();
+});
+
+app.get('/big-ass-game-state', function (req, res) {
+
+})
+
+app.post('/play-game', function (req, res) {
+    var username = req.cookies.username;
+    res.cookie('playertype', req.body.usertype, { maxAge: 600000, httpOnly: true});
+    treasureChest.players.forEach((player) => {
+        if(player.name == username) {
+            player.type = req.body.usertype;
+        };
+    });
+    console.log(treasureChest);
+    processedPlayers();
+    res.end();
+});
+
 // $ curl -X POST http://localhost:3001/place-token -d '{"tokenType": "x", "tokenLocation": {"row": 1, "column": 1}}' -H "Content-Type: application/json"
 app.post('/place-token', function (req, res) {
+    // var hesCooking = req.cookies.letHimCook;
+    // if(hesCooking) {
+    //     console.log(`He's cooking ${hesCooking}`);
+    // }
+
     console.log(req.body);
     let result = placeToken(req.body.tokenType, req.body.tokenLocation.row, req.body.tokenLocation.column);
     if (result == true) {
@@ -146,3 +192,36 @@ function transitionState() {
     else phase = 'PlayerInput';
 }
 
+function processedPlayers() {
+    let count = 0;
+    treasureChest.players.forEach((player) => {
+        if(player.type == "o") {
+            console.log(player.name);
+        }
+        else if(player.type == "x") {
+            console.log(player.name);
+        }
+        else if(player.type == "s") {
+            count++;
+        }
+    })
+    console.log(count);
+}
+
+app.get('/validate-player', function (req, res) {
+    
+})
+
+// function playerValidation(validatetype) {
+//     if(validatetype == "o" && tokeno == "available") {
+//         console.log('available'); 
+//         tokeno = 'unavailable';
+//     }
+//     if(validatetype == "x" && tokenx == "available") {
+//         console.log('available');
+//         tokenx = 'unavailable'
+//     }
+//     else {
+//         console.log('unavailable')
+//     }
+// }
